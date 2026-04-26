16e41d111f24d49d16cb662c17b20233d5a1b80d
NHYC <154051668+NHYCRaymond@users.noreply.github.com>
2026-04-26 19:54:51 +0800

    feat(layout): edgy.nvim docks (left/right/bottom) for IDEA-style tool windows

---
+++ lua/plugins/ui-layout.lua
@@ -0,0 +1,60 @@
+return {
+  {
+    "folke/edgy.nvim",
+    event = "VeryLazy",
+    init = function()
+      vim.opt.laststatus = 3
+      vim.opt.splitkeep = "screen"
+    end,
+    opts = {
+      animate = {
+        enabled = true,
+        fps = 100,
+        cps = 120,
+      },
+      wo = {
+        winbar = true,
+        winfixwidth = true,
+        winfixheight = false,
+        winhighlight = "WinBar:EdgyWinBar,Normal:EdgyNormal",
+      },
+      keys = {
+        ["q"] = function(win) win:close() end,
+        ["<c-w>>"] = function(win) win:resize("width", 2) end,
+        ["<c-w><lt>"] = function(win) win:resize("width", -2) end,
+        ["<c-w>+"] = function(win) win:resize("height", 2) end,
+        ["<c-w>-"] = function(win) win:resize("height", -2) end,
+        ["<c-w>="] = function(win) win.view.edgebar:equalize() end,
+      },
+      bottom = {
+        { ft = "toggleterm",      size = { height = 0.3 } },
+        { ft = "trouble",         size = { height = 0.3 } },
+        { ft = "qf",              title = "QuickFix",       size = { height = 0.3 } },
+        { ft = "OverseerList",    title = "Run / Tasks",    size = { height = 0.3 } },
+        { ft = "dap-repl",        title = "Debug REPL",     size = { height = 0.3 } },
+        { ft = "dapui_console",   title = "Debug Console",  size = { height = 0.3 } },
+        { ft = "neotest-output",  title = "Tests Output",   size = { height = 0.3 } },
+        { ft = "noice",           size = { height = 0.3 }, filter = function(buf, win) return vim.api.nvim_win_get_config(win).relative == "" end },
+      },
+      left = {
+        { title = "Explorer",  ft = "snacks_layout_box" },
+        { ft = "snacks_picker_list", title = "Picker", size = { height = 0.3 } },
+        { ft = "Outline",      title = "Structure",   pinned = true, open = "Outline", size = { height = 0.5 } },
+        { ft = "dapui_scopes",      title = "Scopes",      size = { height = 0.25 } },
+        { ft = "dapui_breakpoints", title = "Breakpoints", size = { height = 0.25 } },
+        { ft = "dapui_stacks",      title = "Call Stack",  size = { height = 0.25 } },
+        { ft = "dapui_watches",     title = "Watches",     size = { height = 0.25 } },
+      },
+      right = {
+        { title = "Avante", ft = "Avante",      size = { width = 0.35 } },
+        { title = "Help",   ft = "help",        size = { width = 80 } },
+      },
+      options = {
+        left   = { size = 40 },
+        bottom = { size = 12 },
+        right  = { size = 50 },
+        top    = { size = 10 },
+      },
+    },
+  },
+}
