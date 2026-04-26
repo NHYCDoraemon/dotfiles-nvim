bf8a39108f1f814a6122bbb2a3a61459a28062ed
NHYC <154051668+NHYCRaymond@users.noreply.github.com>
2026-04-26 20:00:58 +0800

    feat(minimap): neominimap.nvim with diagnostic/git/search markers

---
+++ lua/plugins/ui-minimap.lua
@@ -0,0 +1,44 @@
+return {
+  {
+    "Isrothy/neominimap.nvim",
+    version = "v3.*.*",
+    enabled = true,
+    lazy = false,
+    keys = {
+      { "<leader>um", "<cmd>Neominimap toggle<cr>", desc = "Toggle Minimap (global)" },
+    },
+    init = function()
+      vim.opt.wrap = false
+      vim.opt.sidescrolloff = 36
+
+      vim.g.neominimap = {
+        auto_enable = true,
+        layout = "split",
+        split = {
+          direction = "right",
+          width = 14,
+          close_if_last_window = true,
+        },
+        click = { enabled = true, swap_buffer_with_click = false },
+        diagnostic = {
+          enabled = true,
+          severity = vim.diagnostic.severity.WARN,
+          mode = "icon",
+        },
+        git = { enabled = true, mode = "sign" },
+        search = { enabled = true, mode = "icon" },
+        mark = { enabled = false },
+        treesitter = { enabled = true },
+        winopt = function(opt)
+          opt.winhl = "Normal:NeominimapBackground,FloatBorder:NeominimapBorder"
+          opt.signcolumn = "no"
+          opt.foldcolumn = "0"
+        end,
+        x_multiplier = 4,
+        y_multiplier = 1,
+        exclude_filetypes = { "help", "neo-tree", "snacks_layout_box", "Outline", "dapui_scopes", "dapui_breakpoints", "dapui_stacks", "dapui_watches", "Avante", "AvanteInput" },
+        exclude_buftypes = { "nofile", "nowrite", "quickfix", "terminal", "prompt" },
+      }
+    end,
+  },
+}
